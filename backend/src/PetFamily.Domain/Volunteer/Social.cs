3447473b9c3a8ecf4b308b2f2073d0a54bcd0617
netordot <sdenisdv1@gmail.com>
2024-09-19 18:54:28 +0300

    Volunteer model added

---
+++ backend/src/PetFamily.Domain/Volunteer/Social.cs
@@ -0,0 +1,7 @@
+﻿namespace PetFamily.Domain.Volunteer;
+
+public class Social
+{
+    public string Name { get; }
+    public string Link { get; }
+}
---
+++ backend/src/PetFamily.Domain/Volunteer/Volunteer.cs
@@ -0,0 +1,26 @@
+﻿namespace PetFamily.Domain.Volunteer;
+
+
+public class Volunteer
+{
+    public Guid Id { get;}
+
+    public string FullName { get; }
+
+    public string Email { get; }
+    public string Description { get; }
+    public int Experience { get;}
+    public int PhoneNumber { get; }
+
+    public int PetsRequireHome { get; }
+    public int PetsAdopted { get; } 
+    public int PetsOnTreatment { get; }
+
+    public List<Pet> Pets { get; } = [];
+    public List<Requisite> Requisites { get; } = [];
+    public List<Social> Socials { get; } = [];
+    
+
+
+
+}
