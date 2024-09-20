5ca8f0e0a5a8cf309cb789b581cc4468e05105cf
netordot <sdenisdv1@gmail.com>
2024-09-20 13:36:45 +0300

    Added PetPhoto

---
+++ backend/src/PetFamily.Domain/Pet/PetPhoto.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain;
+
+public class PetPhoto : ValueObject
+{
+    public string Path { get; }
+    public bool isMain { get; }
+
+    private  PetPhoto(string path, bool isMain)
+    {
+        Path = path;
+        this.isMain = isMain; 
+    }
+
+    public static Result<PetPhoto> Create(string path, bool isMain)
+    {
+        return new PetPhoto(path, isMain);  
+    }
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Path;
+        yield return isMain;
+    }
+}
