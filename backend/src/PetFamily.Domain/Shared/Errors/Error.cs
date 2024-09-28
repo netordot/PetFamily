3d0ef53b9a4ce341a089a40e47ba845a438a16a6
netordot <sdenisdv1@gmail.com>
2024-09-28 14:10:27 +0300

    Added Errors

---
+++ backend/src/PetFamily.Domain/Shared/Errors/Error.cs
@@ -0,0 +1,32 @@
+﻿namespace PetFamily.Domain.Shared.Errors;
+
+public record Error
+{
+    public string Code { get;}
+    public string Message { get;}
+    public ErorType Type { get;}
+
+    private Error(string code, string message, ErorType type)
+    {
+        Code = code;
+        Message = message;
+        Type = type;
+    }
+    
+    
+    public static Error Validation(string code, string message) => new Error(code, message, ErorType.Validation);
+    public static Error NotFound(string code, string message) => new Error(code, message, ErorType.NotFound);
+    public static Error Failure(string code, string message) => new Error(code, message, ErorType.Failure);
+    public static Error Conflict(string code, string message) => new Error(code, message, ErorType.Conflict);
+    
+}
+
+
+
+public enum ErorType
+{
+    Validation,
+    NotFound,
+    Failure,
+    Conflict,
+}
---
+++ backend/src/PetFamily.Domain/Shared/Errors/Errors.cs
@@ -0,0 +1,25 @@
+﻿namespace PetFamily.Domain.Shared.Errors;
+
+public static class Errors
+{
+    public static class General
+    {
+        public static Error ValueIsInvalid(string? name = null)
+        {
+            var label = name ?? "value";
+            return Error.Validation("value.is.invalid", $"{label} is invalid");
+        }
+
+        public static Error NotFound(Guid? id = null)
+        {
+            var forId = id == null ? "" : $"for id '{id}'";
+            return Error.NotFound("record.not.found", $"record not found {forId}");
+        }
+
+        public static Error ValueIsRequired(string? name = null)
+        {
+            var label = name == null ? "" : " " + name + " ";
+            return Error.Validation("length.is.invalid", $"invalid{label}");
+        }
+    }
+}
